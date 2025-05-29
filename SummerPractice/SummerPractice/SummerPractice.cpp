06c20acf89c921932bcbae26994da2370d2853be
MoeruKaze <74502840+MoeruKaze@users.noreply.github.com>
2025-05-29 17:27:19 +0300

    Add files via upload

---
+++ SummerPractice/SummerPractice/SummerPractice.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "RedBlackTree.h"
+#include "locale.h"
+int main() {
+    setlocale(LC_ALL, "RU");
+    RedBlackTree<int> tree;
+
+    // Добавляем элементы в дерево
+    tree.add(10);
+    tree.add(5);
+    tree.add(15);
+    tree.add(3);
+    tree.add(7);
+    tree.add(12);
+    tree.add(20);
+
+    // Выводим структуру дерева
+    std::cout << "Красно-черное дерево (int):\n";
+    std::cout << tree << std::endl;
+
+    // Проверяем наличие элементов
+    std::cout << "Содержит 7: " << (tree.find(7) ? "Да" : "Нет") << std::endl;
+    std::cout << "Содержит 8: " << (tree.find(8) ? "Да" : "Нет") << std::endl;
+
+    return 0;
+}
